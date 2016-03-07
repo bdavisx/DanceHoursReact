2c86f1e0c9b9c7a841746c4ac5b8917c4835a492
Bill Davis <bdavisx@yahoo.com>
2016-03-07 16:24:11 -0600

    Initial webpack build working correctly.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        port: 3333
+    },
+    entry: './src/main.jsx',
+    output: { path: './dist/', filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+};
